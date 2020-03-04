116748b2f5d0ef534a78f45e05286e7b7ca6adf3
Bilguun Nomch <bbg.1015@gmail.com>
2020-03-03 22:27:01 -0600

    added skills page

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import * as emailjs from 'emailjs-com';
 // import { withFormik, Form, Field } from 'formik';
 
@@ -21,36 +21,37 @@ const ContactMe = props => {
     const [success, setSuccess] = useState(false)
 
     const handleChange = e => {
-        setMessage({...message, 
+        setMessage({
+            ...message,
             [e.target.name]: e.target.value
         });
     };
 
     const handleSubmit = e => {
         e.preventDefault();
-        if(message.sender.length < 1 || !message.sender){
+        if (message.sender.length < 1 || !message.sender) {
             setError({
                 ...error,
                 sender: true
             })
         }
-        if(message.email.length < 1 || !message.email){
+        if (message.email.length < 1 || !message.email) {
             setError({
                 ...error,
                 email: true
             })
         }
-        if(message.text.length < 10 || !message.text){
+        if (message.text.length < 10 || !message.text) {
             setError({
                 ...error,
                 text: true
             })
         }
-        if(!message.email && !message.sender && !message.text){
+        if (!message.email && !message.sender && !message.text) {
             setError({
                 sender: true,
-                email:true,
-                text:true
+                email: true,
+                text: true
             })
         }
         if (message.email && message.sender && message.text) {
@@ -66,7 +67,7 @@ const ContactMe = props => {
                 templateParams,
                 process.env.REACT_APP_EMAILJS_USER_ID
             ).then(res => {
-                if(res.status === 200){
+                if (res.status === 200) {
                     console.log(res)
                     setSuccess(true)
                 }
@@ -80,9 +81,9 @@ const ContactMe = props => {
     const handleReset = e => {
         e.preventDefault();
         setMessage({
-            sender:'',
-            email:'',
-            text:''
+            sender: '',
+            email: '',
+            text: ''
         })
     }
 
@@ -91,40 +92,40 @@ const ContactMe = props => {
         <div className='contact'>
             <div className='header'>
                 <h2>Contact</h2>
-                <Icon name='cancel' size='large' onClick={()=>{props.setShowContact(false)}}/>
+                <Icon name='cancel' size='large' onClick={() => { props.setShowContact(false) }} />
             </div>
-            {!success ? 
-            <div className='send_form'>
-            <div className='labels'>
-                <label>Name</label>
-                <label>Email</label>
-            </div>
-            <div className='inputs'>
-                <input type='text' name='sender' placeholder = {error.sender ? 'Name is required': null} value={message.sender} onChange={handleChange}/>
-                <input type='email' name='email' placeholder = {error.email ? 'Email is required': null} value={message.email} onChange={handleChange} required/>
-            </div>
-            <div className='labels'>
-            <label>Message</label>
-            </div>
-            <div className='inputs big'> 
-                <textarea name='text' placeholder = {error.text ? 'Message is required': null} value={message.text} onChange={handleChange}>
-                </textarea>
-            </div>
-            <div className='buttons'>
-                <button id='submit' type='submit' onClick={handleSubmit}>SEND MESSAGE</button>
-                <button id='reset' type='reset' onClick={handleReset}>RESET</button>
-            </div>
-            <div className='links'>
-                <a href='https://github.com/Bilguun1015' target='_blank'><Icon name='github' size='big' /></a>
-                <a href='https://www.linkedin.com/in/bilguun-nomch/' target='_blank'><Icon name='linkedin' size='big' /></a>
-                <a href='mailto:bbg.1015@gmail.com'><Icon name='mail' size='big' target='_blank'/></a>
-            </div>
-            </div> : <>
+            {!success ?
+                <div className='send_form'>
+                    <div className='labels'>
+                        <label>Name</label>
+                        <label>Email</label>
+                    </div>
+                    <div className='inputs'>
+                        <input type='text' name='sender' placeholder={error.sender ? 'Name is required' : null} value={message.sender} onChange={handleChange} />
+                        <input type='email' name='email' placeholder={error.email ? 'Email is required' : null} value={message.email} onChange={handleChange} required />
+                    </div>
+                    <div className='labels'>
+                        <label>Message</label>
+                    </div>
+                    <div className='inputs big'>
+                        <textarea name='text' placeholder={error.text ? 'Message is required' : null} value={message.text} onChange={handleChange}>
+                        </textarea>
+                    </div>
+                    <div className='buttons'>
+                        <button id='submit' type='submit' onClick={handleSubmit}>SEND MESSAGE</button>
+                        <button id='reset' type='reset' onClick={handleReset}>RESET</button>
+                    </div>
+                    <div className='links'>
+                        <a href='https://github.com/Bilguun1015' target='_blank'><Icon name='github' size='big' /></a>
+                        <a href='https://www.linkedin.com/in/bilguun-nomch/' target='_blank'><Icon name='linkedin' size='big' /></a>
+                        <a href='mailto:bbg.1015@gmail.com'><Icon name='mail' size='big' target='_blank' /></a>
+                    </div>
+                </div> : <>
                     <h3 id='message'>Thank you for your message!</h3>
                     {/* <button>back</button> */}
-                    </>
+                </>
             }
-            
+
         </div>
         // </form>
     )
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,14 +1,52 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import { Icon } from 'semantic-ui-react';
 
 import './sidebar.scss';
 
 const Skills = props => {
-    return(
+    return (
         <div className='skills'>
             <div className='header'>
                 <h2>Skills</h2>
-                <Icon name='cancel' size='large' onClick={()=>{props.setShowSkills(false)}}/>
+                <Icon name='cancel' size='large' onClick={() => { props.setShowSkills(false) }} />
+            </div>
+            <div className='skill_icons'>
+                <div className = 'each_icon'>
+                    <Icon name='python' size='big' />
+                    <h4>Python</h4>
+                </div>
+                <div className = 'each_icon'>
+                    <Icon name='database' size='big' />
+                    <h4>SQL</h4>
+                </div>
+                <div className = 'each_icon'>
+                    <Icon name='js' size='big' />
+                    <h4>JavaScript</h4>
+                </div>
+                <div className = 'each_icon'>
+                    <Icon name='html5' size='big' />
+                    <h4>HTML</h4>
+                </div>
+                <div className = 'each_icon'>
+                    <Icon name='css3' size='big' />
+                    <h4>CSS</h4>
+                </div>
+                <div className = 'each_icon'>
+                    <Icon name='sass' size='big' />
+                    <h4>Sass</h4>
+                </div>
+                <div className = 'each_icon'>
+                    <Icon name='less' size='big' />
+                    <h4>Less</h4>
+                </div>
+                <div className = 'each_icon'>
+                    <Icon name='node js' size='big' />
+                    <h4>Node.JS</h4>
+                </div>
+                <div className = 'each_icon'>
+                    <Icon name='react' size='big' />
+                    <h4>React.JS</h4>
+                </div>
             </div>
         </div>
     )

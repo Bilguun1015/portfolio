111c0960645271c290a8701e306b8713293ba2ba
Bilguun Nomch <bbg.1015@gmail.com>
2020-03-11 18:56:02 -0500

    working on loading page

---
+++ src/pages/Loading.js
@@ -0,0 +1,13 @@
+import React,{ useState } from 'react';
+
+
+const Loading = props => {
+
+    return(
+        <>
+        <h1>This will show when the page is loading</h1>
+        </>
+    )
+}
+
+export default Loading

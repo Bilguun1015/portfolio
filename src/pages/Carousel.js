309bf9e8fbf597e27655bb9e7f8507065622f848
Bilguun <bbg.1015@gmail.com>
2020-07-06 14:49:59 -0500

    working on project carousel, using React Bootstrap carousel.

--- src/pages/Carousel.js
+++ src/pages/Carousel.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { Carousel, Caption, Item } from 'react-bootstrap';
 
-import speakoutproj from './pics/speakoutproj.jpg';
+import speakoutproj from './pics/speakout.png';
 import secretrecipeproj from './pics/secretrecipeproj.jpg';
 import nodejs from './pics/nodejs.png';
 
@@ -15,7 +15,7 @@ function Projects() {
     };
   
     return (
-      <Carousel activeIndex={index} onSelect={handleSelect} indicators = {false} pause='hover'>
+      <Carousel activeIndex={index} onSelect={handleSelect} indicators = {true} pause='hover' interval={null}>
         <Carousel.Item>
           <img
             className="d-block w-100"
@@ -23,14 +23,29 @@ function Projects() {
             alt="First slide"
           />
           <Carousel.Caption>
+          <h4>Speak Out</h4> 
+          <p>Worked on a solution for English School microsoft database migration to a web based database</p>
+          <ul>
+            <li>Did user research and competitive research during the planning phase</li>
+            <li>Built out the front end view for students’ information using React framework</li>
+            <li>Implemented attendance taking functionality and CRUD methods</li>
+            <li>For state management, implemented Redux store</li>
+
+          </ul>
+          </Carousel.Caption>
+          
+          {/* <button>Click me here</button>
+            <button>Click me here</button> */}
+          {/* <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-          <Carousel.Caption>
+          </Carousel.Caption> */}
+          {/* <Carousel.Caption>
             <button>Click me here</button>
             <button>Click me here</button>
-          </Carousel.Caption>
+          </Carousel.Caption> */}
         </Carousel.Item>
+
         <Carousel.Item>
           <img
             className="d-block w-100"
